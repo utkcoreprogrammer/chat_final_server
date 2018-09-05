3fd4d79794e66afce7c2f14fba48d65f7919e368
utkcoreprogrammer <utkarsshukla@gmail.com>
2018-09-05 19:10:04 +0530

    group chat API's office

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const port = 9090;
 var users;
 var count;
 var chatRooms;
+var groups;
 var messages = [];
 var messageArray = [];
 var chatHistory = [];
@@ -31,63 +32,67 @@ MongoClient.connect('mongodb://localhost:27017/Chat_App', (err, Database) => {
         console.log(err);
         return false;
     }
-    console.log("Connected to MongoDB");
-    const db = Database.db("Chat_App");
-    users = db.collection("users");
-    chatRooms = db.collection("chatRooms");
-    const server = app.listen(port, () => {
+    else{
+        console.log("Connected to MongoDB");
+        const db = Database.db("Chat_App");
+        users = db.collection("users");
+        chatRooms = db.collection("chatRooms");
+        groups = db.collection("groups");
+        const server = app.listen(port, () => {
         console.log("Server started on port " + port + "...");
-    });
-    io = socket.listen(server);
-
-    io.sockets.on('connection', (socket) => {
-        console.log("socket connected>>>>", socket.id);
-        socket.on('join', (data) => {
-            socket.join(data.room);
-            console.log("data>>>>", data);
-            chatRooms.find({}).toArray((err, rooms) => {
-                if(err){
-                    console.log("err>>>>>", err);
-                    return false;
-                }
-                count = 0;
-                console.log("rooms>>>>" ,rooms);
-                rooms.forEach((room) => {
-                    if(room.name == data.room){
-                        console.log("room >>>", room);
-                        count++;
-                        messageArray = room.messages
-                        // console.log("messageArray>>>>>>", messageArray)
-                        // socket.emit('getMessages', messageArray);
+        });
+        io = socket.listen(server);
+        io.sockets.on('connection', (socket) => {
+            console.log("socket connected>>>>", socket.id);
+            socket.on('join', (data) => {
+                socket.join(data.room);
+                console.log("data>>>>", data);
+                chatRooms.find({}).toArray((err, rooms) => {
+                    if(err){
+                        console.log("err>>>>>", err);
+                        return false;
                     }
+                    else{
+                        count = 0;
+                        console.log("rooms>>>>" ,rooms);
+                        rooms.forEach((room) => {
+                            if(room.name == data.room){
+                            console.log("room >>>", room);
+                            count++;
+                            messageArray = room.messages
+                            console.log("count$#%%%#$%", count);
+                            // console.log("messageArray>>>>>>", messageArray)
+                            // socket.emit('getMessages', messageArray);
+                            }
+                        });
+                        if(count == 0) {
+                        console.log("count insert one >>>");
+                        chatRooms.insertOne({ name: data.room, messages: [] }); 
+                        }
+                    }
+                    
                 });
-                if(count == 0) {
-                    console.log("count insert one >>>");
-                    chatRooms.insertOne({ name: data.room, messages: [] }); 
-                }
+            });
+            socket.on('message', (data) => {
+                console.log("message from server@!#@#!#!$!$$$$", data);
+                io.in(data.room).emit('new message', {user: data.user, message: data.message});
+                chatRooms.updateOne({name: data.room}, { $push: { messages: { user: data.user, message: data.message } } }, (err, res) => {
+                    if(err) {
+                        console.log(err);
+                        return false;
+                    }
+                    else{
+                        console.log("Document updated");
+                    }
+                    
+                });
+            });
+            socket.on('typing', (data) => {
+                console.log("data from typing server&&&&&&&&&&", data);
+                socket.broadcast.in(data.room).emit('typing', {data: data, isTyping: true});            
             });
         });
-        socket.on('message', (data) => {
-            console.log("message from server@!#@#!#!$!$$$$", data);
-            io.in(data.room).emit('new message', {user: data.user, message: data.message});
-            chatRooms.updateOne({name: data.room}, { $push: { messages: { user: data.user, message: data.message } } }, (err, res) => {
-                if(err) {
-                    console.log(err);
-                    return false;
-                }
-                else{
-                    console.log("Document updated");
-                }
-                
-            });
-        });
-        socket.on('typing', (data) => {
-            console.log("data from typing server&&&&&&&&&&", data);
-            socket.broadcast.in(data.room).emit('typing', {data: data, isTyping: true});
-            // addEventListener("keypress")
-
-        });
-    });
+    }
 
 }); 
 
@@ -105,7 +110,6 @@ app.get('/chatroom/:room', (req, res, next) => {
         }
         else{
         console.log("getting chatroom", chatroom);
-        console.log("chatHistory>>>>", chatHistory);
         res.status(200).json(chatroom);
         }
     });
@@ -126,24 +130,71 @@ app.post('/user/register', (req, res, next) => {
             console.log(err);
             return res.status(500).send(err);
         }
-        console.log("Users.find>>>>>>>");
-        for(let i = 0; i < Users.length; i++){
-            if(Users[i].username == user.username)
-                count++;
+        else{
+            console.log("Users.find>>>>>>>");
+            for(let i = 0; i < Users.length; i++){
+                if(Users[i].username == user.username)
+                    count++;
+            }
+            // Add user if not already signed up
+            if(count == 0){
+                users.insertOne(user, (err, User) => {
+                    if(err){
+                        res.send(err);
+                    }
+                    else{
+                    res.json(User);
+                    }
+                });
+            }
+            else {
+                // Alert message logic here
+                res.json({ user_already_signed_up: true });
+            }    
         }
-        // Add user if not already signed up
-        if(count == 0){
-            users.insertOne(user, (err, User) => {
-                if(err){
-                    res.send(err);
-                }
-                res.json(User);
-            });
+    
+    });
+    
+});
+
+app.post('/user/group', (req, res, next) => {
+    console.log("inside user/group>>>", req.body);
+    console.log("req.body>>>>>>>>", req.body);
+    let group = req.body;
+      
+    groups.find({}).toArray((err, Groups) => {
+        if (err) {
+            console.log("err>>>> ",err);
+            return res.status(500).send(err);
         }
-        else {
-            // Alert message logic here
-            res.json({ user_already_signed_up: true });
+        else{
+                let count = 0; 
+                console.log("No error in groups find");
+                Groups.forEach((group) => {
+                    if(group.groupName === req.body.groupName){
+                    count++;
+                    }
+                });
+                   console.log("count$#%%%#$%", count);
+            // Add group if not already created
+            if(count == 0){
+                groups.insertOne(group, (err, User) => {
+                    if(err){
+                        console.log("unable to insert");
+                        res.status(500).send(err);
+                    }
+                    else{
+                    console.log("group created", User.ops[0]);
+                    res.status(200).json(User);
+                    }
+                });
+            }
+            else {
+                console.log("group exists");
+                res.json({ group_already_exists: true });
+            }    
         }
+    
     });
     
 });
@@ -167,13 +218,11 @@ app.post('/user/auth', (req, res) => {
 
             users.findOneAndUpdate({"email" : req.body.email, "password" : req.body.password}, {$set : {isOnline : true}}, {returnOriginal: false},(err,loggedInUser) =>
             {   
-                console.log("found user login$##$##$#", loggedInUser)
                 if(err){
                 console.log("error in find and update", err);   
                 }
                 else{
                     console.log("loggedInUser", loggedInUser.value);
-                    console.log("loggedInUser", loggedInUser);
                     if(!loggedInUser.value){
                     console.log("Both credentials are not true");  
                     }
@@ -183,8 +232,6 @@ app.post('/user/auth', (req, res) => {
                     console.log("user found and authorized>>>>>>", loggedInUser.value) 
                     io.emit("logged_in_user",loggedInUser.value);   
                     res.status(200).json({ isPresent: isPresent, correctPassword: correctPassword, user: loggedInUser.value });
-
-
                     }
                        
                 }
@@ -209,36 +256,45 @@ app.post('/user/logOut', (req, res, next) =>
   let email = req.body.email;
   console.log("email from api@@@", email);
 
-  users.findOneAndUpdate({"email" : req.body.email}, {$set : {isOnline : false}}, {returnOriginal: false}, (err, foundUser) =>
-  {
-    console.log("foundUser logout>>>>>", foundUser);
-    if(err){
-        console.log("err in user logOut", err);
-    }
-    else{
-        // if(!foundUser.value){
-        console.log("logout user>>>>>>>", foundUser);
-        // }
+    users.findOneAndUpdate({"email" : req.body.email}, {$set : {isOnline : false}}, {returnOriginal: false}, (err, foundUser) =>
+    {
+        console.log("foundUser logout>>>>>", foundUser);
+        if(err){
+            console.log("err in user logOut", err);
+        }
+        else{
+            // if(!foundUser.value){
+            console.log("logout user>>>>>>>", foundUser);
+            // }
 
-        io.emit("log_Out_User",foundUser.value);   
-        res.status(200).json(foundUser.value);
-    }
-})
+            io.emit("log_Out_User",foundUser.value);   
+            res.status(200).json(foundUser.value);
+        }
+    })
 
 });
 
 app.get('/user/getAllUsers', (req, res, next) => {
-    users.find({}, {username: 1, email: 1, _id: 0}).toArray((err, users) => {
+    users.find({}).toArray((err, users) => {
         if(err) {
 
             res.send(err);
         }
         else
-            res.json(users);
+            res.status(200).json(users);
     });
 });
 
+app.get('/user/getAllGroups', (req, res) => {
+    groups.find({}).toArray((err, groups) => {
+        if(err) {
 
-
+            res.send(err);
+        }
+        else
+            console.log("groups", groups)
+            res.status(200).json(groups);
+    });
+});
 
 
